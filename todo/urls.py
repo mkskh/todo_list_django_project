1f7eaa58483762ccbacd8f75ddcfc417b52c199f
mkskh <maksym.kharchenko@dci-student.org>
2024-03-02 21:25:33 +0100

    implemented: register system; system where each user owns their own individual notes

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
