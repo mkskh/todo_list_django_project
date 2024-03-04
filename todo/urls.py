8fd34114f63dc7683784394a66827946fef66629
mkskh <maksym.kharchenko@dci-student.org>
2024-03-04 01:53:58 +0100

    added style. FINISHED project

--- todo/urls.py
+++ todo/urls.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
